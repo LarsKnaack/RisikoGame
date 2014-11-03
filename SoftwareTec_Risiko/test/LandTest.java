e9748a1904aebc6cb37cf728844cc280bec08278
Lars <lars.knaack@transdocu.de>
2014-11-03 08:32:04 +0100

    JUnit LandTest added

--- SoftwareTec_Risiko/test/LandTest.java
+++ SoftwareTec_Risiko/test/LandTest.java
@@ -1,4 +1,32 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 public class LandTest {
 
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public int testGetAnzahlEinheiten() {
+		Land.setEinheiten(5);
+		assertTrue(Land.getEinheiten() == 5);
+	}
+	
+	public void testSetEinheiten(int einheiten) {
+		Land.setEinheiten(einheiten);
+		assertTrue(Land.getEinheiten() == einheiten);
+	}
+	
+	public void testGetBesatzer() {
+		Spieler s3 = new Spieler();
+		Land.setBesatzer(s3);
+		assertTrue(Land.getEinheiten() == s3);
+	}
+	
+	public void testSetBesatzer(Spieler s) {
+		Land.setBesatzer(s);
+		assertTrue(Land.getBesatzer() == s);
+	}
 }

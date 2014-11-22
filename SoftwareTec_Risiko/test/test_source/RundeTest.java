ac995781f039d7ad73c13262109136107e4bd9db
Lars <lars.knaack@transdocu.de>
2014-11-22 13:21:25 +0100

    removed randomization

--- SoftwareTec_Risiko/test/test_source/RundeTest.java
+++ SoftwareTec_Risiko/test/test_source/RundeTest.java
@@ -1,25 +1,36 @@
 package test_source;
 import static org.junit.Assert.*;
+
 import java.awt.Color;
+import java.lang.reflect.Field;
+import java.util.Random;
+
 import junit.framework.TestCase;
+
+import org.junit.Before;
 import org.junit.Test;
+
 import source.Spieler;
 import source.Land;
 import source.Runde;
 
 
 public class RundeTest extends TestCase {
-
+	
 	public void testWuerfeln() {
 		Runde r = new Runde();
 		Land ang = new Land("Angreifer");
+		ang.setEinheiten(5);
 		Land dev = new Land("Verteidiger");
+		dev.setEinheiten(1);
 		Spieler s1 = new Spieler("s1", Color.BLACK);
 		Spieler s2 = new Spieler("s2", Color.BLUE);
 		ang.setBesatzer(s1);
 		dev.setBesatzer(s2);
 		ang.setAngreifer(true);
 		dev.setVerteidiger(true);
+		r.setWuerfel(ang);
+		r.setWuerfel(dev);
 		Spieler gewinner = r.wuerfeln(ang, dev);
 		assertTrue(gewinner == s1 || gewinner == s2);
 	}

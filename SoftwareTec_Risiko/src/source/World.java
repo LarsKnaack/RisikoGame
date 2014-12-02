6287d74fbe37f5949f5e7e496b6d59398df088dc
heettwei <heettwei@3DLAB9.htwg-konstanz.de>
2014-12-02 11:57:04 +0100

    created getter and setter for map in world.java

--- SoftwareTec_Risiko/src/source/World.java
+++ SoftwareTec_Risiko/src/source/World.java
@@ -10,28 +10,36 @@ public class World implements WorldGraph{
 	private Map<Country, LinkedList<Country>> world;
 
 	public World() {
-		world = new HashMap<Country, LinkedList<Country>>();
+		setWorld(new HashMap<Country, LinkedList<Country>>());
 	}
 
 	@Override
 	public void addCountry(Country v) {
-		world.put(v, new LinkedList<Country>());	
+		getWorld().put(v, new LinkedList<Country>());	
 	}
 
 	@Override
 	public void addEdge(Country v, Country w) {
-		world.get(v).add(w);
-		world.get(w).add(v);
+		getWorld().get(v).add(w);
+		getWorld().get(w).add(v);
 	}
 
 	@Override
 	public List<Country> getNeighbouringCountryList(Country v) {
-		return world.get(v);
+		return getWorld().get(v);
 	}
 
 	@Override
 	public boolean isNeighbour(Country v, Country w) {
 		return getNeighbouringCountryList(v).contains(w);
+	}
+
+	public Map<Country, LinkedList<Country>> getWorld() {
+		return world;
+	}
+
+	public void setWorld(Map<Country, LinkedList<Country>> world) {
+		this.world = world;
 	}
 
 }
--- SoftwareTec_Risiko/test/test_source/WorldTest.java
+++ SoftwareTec_Risiko/test/test_source/WorldTest.java
@@ -10,7 +10,7 @@ public class WorldTest extends TestCase {
 		World world = new World();
 		Country test = new Country("test");
 		world.addCountry(test);
-		assertTrue(world.world.containsKey(test));
+		assertTrue(world.getWorld().containsKey(test));
 	}
 
 	public void testAddEdge() {
@@ -20,8 +20,8 @@ public class WorldTest extends TestCase {
 		world.addCountry(test1);
 		world.addCountry(test2);
 		world.addEdge(test1, test2);
-		assertTrue(world.world.get(test1).contains(test2));
-		assertTrue(world.world.get(test2).contains(test1));
+		assertTrue(world.getWorld().get(test1).contains(test2));
+		assertTrue(world.getWorld().get(test2).contains(test1));
 	}
 
 	public void testGetNeighbouringCountryList() {
@@ -32,7 +32,7 @@ public class WorldTest extends TestCase {
 		world.addCountry(test2);
 		world.addEdge(test1, test2);
 		world.getNeighbouringCountryList(test1);
-		assertTrue(world.getNeighbouringCountryList(test1).equals(world.world.get(test1)));
+		assertTrue(world.getNeighbouringCountryList(test1).equals(world.getWorld().get(test1)));
 	}
 
 	public void testIsNeighbour() {

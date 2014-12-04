08ec136f0d5a590c4809c0008e315cd54ccb301b
Lars <lars.knaack@transdocu.de>
2014-12-04 10:39:21 +0100

    Added GameField

---
+++ SoftwareTec_Risiko/de.htwg.risiko.src/field/GameField.java
@@ -0,0 +1,18 @@
+package field;
+
+import entities.Country;
+import world.World;
+
+public class GameField {
+	
+	World gamefield = new World();
+	
+	Country germany = new Country("Germany");
+	Country austria = new Country("Austria");
+	Country france = new Country("France");
+	Country switzerland = new Country("Switzerland");
+	Country uk = new Country("UK");
+	
+	
+
+}

3b50721c5b46308f2e7a055adb7a5d4fb7739679
Lars <lars.knaack@transdocu.de>
2014-12-07 11:49:41 +0100

    Added Interfaces

---
+++ SoftwareTec_Risiko/de.htwg.risiko.src/de/htwg/risiko/model/CountryI.java
@@ -0,0 +1,13 @@
+package de.htwg.risiko.model;
+
+public interface CountryI {
+
+	public int getSoldiers();
+
+	public void setSoldiers(int e);
+
+	public String getName();
+
+	public void setName(String n);
+
+}
---
+++ SoftwareTec_Risiko/de.htwg.risiko.src/de/htwg/risiko/model/PlayerI.java
@@ -0,0 +1,7 @@
+package de.htwg.risiko.model;
+
+public interface PlayerI {
+
+	public String getName();
+
+}

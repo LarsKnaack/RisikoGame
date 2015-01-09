acd667fdb2d5bce93306256cc0569010c18ddbff
Lars <lars.knaack@transdocu.de>
2015-01-09 12:22:42 +0100

    removed Tests

--- SoftwareTec_Risiko/de.htwg.risiko.test/de/htwg/risiko/model/AllTests.java
+++ SoftwareTec_Risiko/de.htwg.risiko.test/de/htwg/risiko/model/AllTests.java
@@ -4,8 +4,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import de.htwg.risiko.turn.DieTest;
-import de.htwg.risiko.turn.InvadeTurnTest;
-import de.htwg.risiko.turn.RecruitmentTurnTest;
 import de.htwg.risiko.turn.StartTurnTest;
 import de.htwg.risiko.turn.TurnTest;
 
@@ -13,7 +11,7 @@ import de.htwg.risiko.turn.TurnTest;
 @RunWith(Suite.class)
 
 @SuiteClasses({CountryTest.class, PlayerTest.class, WorldTest.class, TurnTest.class,
-	InvadeTurnTest.class, StartTurnTest.class, RecruitmentTurnTest.class, DieTest.class})
+	 StartTurnTest.class, DieTest.class})
 
 public class AllTests {
 
